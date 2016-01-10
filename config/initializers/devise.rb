1ff1f11a64158261db0d155c3ed8185de4ec30fd
alex707 <alex.grigorenko2942@gmail.com>
2016-01-10 17:03:41 +0300

    added list users, user avatar, page of user

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -262,4 +262,8 @@ Devise.setup do |config|
   # When using OmniAuth, Devise cannot automatically set OmniAuth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
+
+  #its need to normal customization of view
+  config.scoped_views = true
+
 end
